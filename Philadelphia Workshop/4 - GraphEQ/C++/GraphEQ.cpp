561673b9af92b9673a99df0eb706aac99dbf7bbf
Erik Saule <esaule@uncc.edu>
2018-05-30 19:35:41 -0400

    C++ graph earthquake

--- Philadelphia Workshop/4 - GraphEQ/C++/GraphEQ.cpp
+++ Philadelphia Workshop/4 - GraphEQ/C++/GraphEQ.cpp
@@ -6,6 +6,28 @@
 
 using namespace bridges;
 
+double degree_to_rad(double d) {
+  return d*M_PI/180;
+}
+
+double calcDistance(double la1, double lo1, double la2, double lo2) {
+  double R = 6371; // Radius of the earth in KM
+  // Haversine formula to calculate a value between 0 and 1 between 2 points on a sphere, 1 being the
+  // opposite side of the sphere
+  double laDistance = degree_to_rad(la2 - la1);
+  double loDistance = degree_to_rad(lo2 - lo1);
+  double a = sin(laDistance / 2) * sin(laDistance / 2)
+    + cos(degree_to_rad(la1)) * cos(degree_to_rad(la2))
+    * sin(loDistance / 2) * sin(loDistance / 2);
+
+  double c = 2. * atan2(sqrt(a), sqrt(1 - a));
+
+  
+  double distance = R * c;    //convert to KM
+  return distance;
+}
+
+
 int main() {
 
 	//create the Bridges object, set credentials
@@ -33,14 +55,31 @@ int main() {
 	}
 
 	//Builds a graph with nodes at correct location
-	GraphAdjList<std::string, int, int> graph;
+	GraphAdjList<int, EarthquakeUSGS> graph;
 	
-	for (auto eq : eqs) {
-	  graph.addVertex(eq.getTitle());
-
-	  ElementVisualizer* elv = graph.getVisualizer(eq.getTitle());
+	for (int i=0; i<eqs.size(); ++i) {
+	  auto eq = eqs[i];
+	  graph.addVertex(i, eq);
+	  
+	  Element<EarthquakeUSGS> * el = graph.getVertex(i);
+	  el->setLabel(eq.getTitle());
+	  
+	  ElementVisualizer* elv = graph.getVisualizer(i);
 	  elv->setLocation(eq.getLongit(), eq.getLatit());
-	  elv->setSize (eq.getMagnitude()*4);
+	  elv->setSize (eq.getMagnitude());
+
+	  int red = (int) ((eq.getLongit() / 180.0) * 255);
+	  red = red > 0 ? red : 0;
+	  
+	  int blue = (int) ((eq.getLongit() / 180.0) * 255);
+	  blue = blue < 0 ? blue * -1 : 0;
+	  
+	  int green = (int) ((eq.getLatit() / 90.0) * 255);
+	  green = green < 0 ? green * -1: green;
+
+	  int alpha = 255;
+	  
+	  elv->setColor(Color(red, green, blue, alpha)); 
 	}	
 	
 	// tell Bridges what data structure to visualize
@@ -48,6 +87,31 @@ int main() {
 
 	Bridges::setCoordSystemType("equirectangular");
 	Bridges::setMapOverlay(true);
+
+	// visualize the list
+	Bridges::visualize();
+	
+	//add edges between
+	for (int i=0; i<eqs.size(); ++i) {
+	    EarthquakeUSGS eqi = eqs[i];
+	    for (int j=0; j<eqs.size(); ++j) {
+	      if (i == j) continue;
+	      
+	      EarthquakeUSGS eqj = eqs[j];
+
+	      double distance = calcDistance(eqi.getLatit(), eqi.getLongit(),
+					     eqj.getLatit(), eqj.getLongit());
+
+	      if (distance < 500) {
+		int weight = 1;
+		graph.addEdge(i, j, weight);
+		auto linkvis = graph.getLinkVisualizer(i, j);
+		std::stringstream ss;
+		ss<<distance<<" km";
+		linkvis->setLabel(ss.str());
+	      }
+	  }
+	}
 	
 	// visualize the list
 	Bridges::visualize();
